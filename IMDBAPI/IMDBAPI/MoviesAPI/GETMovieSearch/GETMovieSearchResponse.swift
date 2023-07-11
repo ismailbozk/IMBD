b5348ccae32d6becbb26688219262c5b8345c240
Ismail Bozkurt <ismailbozk@gmail.com>
2023-07-11 18:32:29 +0100

    fixed movie database entity optional type

--- IMDBAPI/IMDBAPI/MoviesAPI/GETMovieSearch/GETMovieSearchResponse.swift
+++ IMDBAPI/IMDBAPI/MoviesAPI/GETMovieSearch/GETMovieSearchResponse.swift
@@ -11,9 +11,9 @@ public struct Movie: Codable, Identifiable, Equatable {
     public let id: Int
     public let title: String
     public let overview: String
-    public let posterPath: String
+    public let posterPath: String?
 
-    public init(id: Int, title: String, overview: String, posterPath: String) {
+    public init(id: Int, title: String, overview: String, posterPath: String?) {
         self.id = id
         self.title = title
         self.overview = overview
